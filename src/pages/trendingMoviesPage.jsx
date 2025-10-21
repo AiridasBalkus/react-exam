de109b793c7c75f90bf7b713791d261c0ed159f3
Airidas Balkus <20107228@mail.wit.ie>
2025-10-21 11:22:30 +0100

    Added Trending This Week Page.

---
+++ src/pages/trendingMoviesPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../components/spinner";
+import PageTemplate from "../components/templateMovieListPage";
+import { getTrendingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+
+const TrendingMoviesPage = () => {
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ['trendingMovies'],
+    queryFn: getTrendingMovies,
+  });
+
+  if (isPending) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
+
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Trending This Week"
+      movies={movies}
+      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+    />
+  );
+};
+
+export default TrendingMoviesPage;
